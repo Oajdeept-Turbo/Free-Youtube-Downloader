5d7707c644d951bd5f17495d0b25e2fbce7e0a38
Oajdeept-Turbo <oajdeept234turbo@hotmail.com>
2025-08-16 08:49:53 +0530

    Downloader.cs

--- Downloader.cs
+++ Downloader.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Diagnostics;
 
-class Downloader
-{
-    static void Main(string[] args)
-    {
-        string url = args.Length > 0 ? args[0] : "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Default URL
-        Process.Start("YTDownloder.exe", $"\"{url}\"");
+class Program {
+    static void Main() {
+        Console.Write("Enter YouTube video URL: ");
+        string url = Console.ReadLine();
+
+        Process.Start(new ProcessStartInfo {
+            FileName = "YTDownloader.exe",
+            Arguments = $"-f \"bv*+ba/best\" --merge-output-format mp4 -o \"%(title)s.%(ext)s\" \"{url}\"",
+            UseShellExecute = false
+        });
+
+        Console.WriteLine("Download started...");
     }
 }
